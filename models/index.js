141682d4980447bfe494666218245fd74b96b968
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-11-08 17:56:28 -0800

    added MongoDB connection to server.js

--- models/index.js
+++ models/index.js
@@ -6,5 +6,6 @@ const db = {};
 db.mongoose = mongoose;
 
 db.user = require("./User");
+db.account = require("./Account");
 
 module.exports = db;
--- server.js
+++ server.js
@@ -3,6 +3,50 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require("./models");
+const dbConfig = require("./config/db.config");
+const Account = db.account;
+
+db.mongoose.connect(`mongodbL//${dbConfig.HOST}/${dbConfig.DB}`, {
+    useNewUrlPaser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => {
+    console.log("Successfully connected to MongoDB!");
+    initial();
+})
+.catch(err => {
+    console.error("Connection Error", err);
+    process.exit();
+});
+
+initial = () => {
+    Account.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Account({
+                username: "Child 1"
+            }).save(err => {
+                if (err) {
+                    console.log(err)
+                }
+
+                console.log("Added 'Child 1' to Accounts collection!")
+            });
+
+            new Account({
+                username: "Child 2"
+            }).save(err => {
+                if (err) {
+                    console.log(err)
+                }
+
+                console.log("Added 'Child 2' to Accounts collection!")
+            });
+        };
+    });
+};
 
 const corsOption = {
     origin: "https//localhost:3001"
